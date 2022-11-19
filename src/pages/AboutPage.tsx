3940d1d30aee28dbef8d72652ca77405cf7f7a72
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2022-11-19 20:51:52 +0400

    basic syntax of typescript code

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage(){
+    return (
+        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt ratione unde earum adipisci aspernatur. Maiores reiciendis tempore, praesentium dolor officia pariatur accusantium est fugit laudantium rerum cupiditate labore, quis molestiae?</div>
+    )
+}
